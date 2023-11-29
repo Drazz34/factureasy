073b3574c3e4bc8b4e178007b0e11fe223648f10
DrazZ34 <jfvillac@hotmail.com>
2023-11-29 16:48:44 +0100

    Lien de la facture vers le client

--- resources/views/factures/show.blade.php
+++ resources/views/factures/show.blade.php
@@ -10,6 +10,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h2 class="font-bold text-lg mb-4">Facture de {{$facture->client->nom}} {{$facture->client->prenom}}</h2>
+                    @if (\Carbon\Carbon::parse($facture->date_echeance)->isPast())
+                    <p class="mb-2 text-red-600 font-bold">Attention : Cette facture est en retard !</p>
+                    @endif
                     <p class="mb-2"><span class="font-bold">Date de facturation :</span> {{ \Carbon\Carbon::parse($facture->date_facture)->format('d/m/Y') }}</p>
                     @if (\Carbon\Carbon::parse($facture->date_echeance)->isPast())
                     <p class="mb-2 text-red-600"><span class="font-bold">Date d'échéance :</span> {{ \Carbon\Carbon::parse($facture->date_echeance)->format('d/m/Y') }}</p>
@@ -19,10 +22,8 @@
                     <p class="mb-2"><span class="font-bold">Total HT :</span> {{$facture->total_ht}} €</p>
                     <p class="mb-2"><span class="font-bold">Total TTC :</span> {{$facture->total_ttc}} €</p>
                     <p class="mb-2"><span class="font-bold">Facture envoyée :</span> {{$facture->facture_envoyee ? 'Oui' : 'Non'}}</p>
-                    <p class="mb-2"><span class="font-bold">Facture payée :</span> {{$facture->facture_payee ? 'Oui' : 'Non'}}</p>
-                    @if (\Carbon\Carbon::parse($facture->date_echeance)->isPast())
-                    <p class="mb-2 text-red-600 font-bold">Attention : Cette facture est en retard !</p>
-                    @endif
+                    <p class="mb-4"><span class="font-bold">Facture payée :</span> {{$facture->facture_payee ? 'Oui' : 'Non'}}</p>
+                    <a href="{{route('clients.show', $facture->client->id)}}" class="btn btn-show">Voir le client</a>
                 </div>
                 <div class="px-6 py-4 flex flex-col md:flex-row justify-end space-y-3 md:space-y-0 md:space-x-3">
                     <x-edit-button :route-name="'factures.edit'" :item-id="$facture->id" />
